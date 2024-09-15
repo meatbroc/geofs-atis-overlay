e56d379077061bafda1805dd3784e296145cf0d0
meatbroc <168386225+meatbroc@users.noreply.github.com>
2024-09-14 20:54:05 -0400

    Update user.js

--- user.js
+++ user.js
@@ -1,1 +1,21 @@
-
+x.registerListener(function (val) {
+    console.log("Airport changed to", val);
+    const req = new XMLHttpRequest();
+    req.addEventListener("load", reqListener);
+    req.open("GET", `https://datis.clowd.io/api/${airportName}`);
+    req.send();
+});
+const x = {
+airportInternal: '',
+airportListener: function (val) { },
+set airport(val) {
+this.airportInternal = val;
+this.airportListener(val);
+},
+get airport() {
+return this.airportInternal;
+},
+registerListener: function (listener) {
+this.airportListener = listener;
+}
+};
