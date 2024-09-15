c7aeedad8d3a3da9db515a7b2dfdae572b283fd4
meatbroc <168386225+meatbroc@users.noreply.github.com>
2024-09-14 22:14:51 -0400

    Update user.js

--- user.js
+++ user.js
@@ -1,21 +1,28 @@
+let airport;
+
+const x = {
+    airportInternal: '',
+    airportListener: function (val) { },
+    set airport(val) {
+        this.airportInternal = val.toUpperCase(); // Convert to uppercase
+        this.airportListener(this.airportInternal); // Pass the uppercase value to the listener
+    },
+    get airport() {
+        return this.airportInternal.toUpperCase();
+    },
+    registerListener: function (listener) {
+        this.airportListener = listener;
+    }
+};
+
 x.registerListener(function (val) {
     console.log("Airport changed to", val);
     const req = new XMLHttpRequest();
     req.addEventListener("load", reqListener);
-    req.open("GET", `https://datis.clowd.io/api/${airportName}`);
+    req.open("GET", `https://datis.clowd.io/api/${val}`);
     req.send();
 });
-const x = {
-airportInternal: '',
-airportListener: function (val) { },
-set airport(val) {
-this.airportInternal = val;
-this.airportListener(val);
-},
-get airport() {
-return this.airportInternal;
-},
-registerListener: function (listener) {
-this.airportListener = listener;
+
+function reqListener() {
+    console.log(this.responseText);
 }
-};
